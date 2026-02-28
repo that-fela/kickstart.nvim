efaede6f15c2ee49be071e155f718b31135b97f8
that-fela <henricoder@gmail.com>
2026-02-28 14:59:26 +1000

    Extracted plugins

---
+++ lua/custom/plugins/colortheme.lua
@@ -0,0 +1,36 @@
+-- return { -- You can easily change to a different colorscheme.
+--   -- Change the name of the colorscheme plugin below, and then
+--   -- change the command in the config to whatever the name of that colorscheme is.
+--   --
+--   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+--   'folke/tokyonight.nvim',
+--   priority = 1000, -- Make sure to load this before all the other start plugins.
+--   config = function()
+--     ---@diagnostic disable-next-line: missing-fields
+--     require('tokyonight').setup {
+--       styles = {
+--         comments = { italic = false }, -- Disable italics in comments
+--       },
+--     }
+--     -- Like many other themes, this one has different styles, and you could load
+--     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+--     -- vim.cmd.colorscheme 'tokyonight-night'
+--   end,
+-- }
+--
+
+return {
+  'Mofiqul/vscode.nvim',
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require('vscode').setup {
+      style = 'dark',
+      transparent = false,
+      italic_comments = true,
+      disable_nvimtree_bg = true,
+    }
+
+    vim.cmd.colorscheme 'vscode'
+  end,
+}
